64564714a31d7aea76a9083094b2e7d304646e32
Wali Rahman <wali.rahman89@yahoo.com>
2017-11-18 16:51:57 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+let responses = emails.map(BatchCreate(userInput)
+
+  return new IcebreakerResponse(userInput[0], userInput[1])
+)}
+}
